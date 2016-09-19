5787cd351c4d61d32a1373e256008a5d4f637ce0
webonise <webonise@webonises-Mac-mini-6.local>
2016-09-19 15:16:05 +0530

    Added main screen

---
+++ SPTuts/SPTuts/SPMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  SPMainViewController.h
+//  SPTuts
+//
+//  Created by webonise on 19/09/16.
+//  Copyright © 2016 webonise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPMainViewController : UIViewController
+
+@end
