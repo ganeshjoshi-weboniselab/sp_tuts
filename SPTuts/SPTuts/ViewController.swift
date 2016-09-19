3de129175217906219fb21580022d329c29c225d
webonise <webonise@webonises-mac-mini-6.local>
2016-09-19 12:44:34 +0530

    1. Swift programming tutorial project with pods added

---
+++ SPTuts/SPTuts/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  SPTuts
+//
+//  Created by webonise on 14/09/16.
+//  Copyright © 2016 webonise. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        var stringHello = "";
+        stringHello = "Hello World!";
+        print(stringHello);
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
